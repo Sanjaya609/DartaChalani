a418a99fbf339f2bce4c7333bbb3b492a80e1a2b
iamsuzeet <iamsuzeet@gmail.com>
2023-11-28 22:23:46 +0545

    wip route privilege

--- src/router/routes/create-route.tsx
+++ src/router/routes/create-route.tsx
@@ -4,82 +4,99 @@ import React from 'react'
 import { _RouteObject } from 'react-router-dom'
 // import { toast } from 'react-toastify'
 import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary'
+import FallbackLoader from '@/components/FallbackLoader'
+import { PRIVILEGEENUM } from '@/utility/enums/privilege.enum'
+import { useAuth } from '@/providers'
+import NotFound from '@/core/NotFound'
+import { Text } from '@/components/ui/core/Text'
 
 export interface CreateRoute<Type extends RouteType>
   extends Omit<_RouteObject<Type>, 'element'> {
   element: React.LazyExoticComponent<React.FC>
   type?: RouteType
+  checkFromParentPath?: RoutePaths
+  checkPrivilege?: PRIVILEGEENUM[]
 }
 
+export interface IRoutePrivilege {
+  routePrivilege: Partial<Record<PRIVILEGEENUM, boolean>>
+}
 interface IPermissionProps {
-  RElement: React.LazyExoticComponent<React.FC>
+  RElement: React.LazyExoticComponent<React.FC<IRoutePrivilege>>
   path: RoutePaths
   checkFromParentPath?: RoutePaths
+  checkPrivilege?: PRIVILEGEENUM[]
 }
 
-// export const Permission = (props: IPermissionProps) => {
-//   const {
-//     RElement,
-//     path,
-//     checkFromParentPath,
-//     checkPrivilege = [PRIVILEGEENUM.READLIST],
-//   } = props
-//   const { flatModulePropsFromURL, initDataFetching } = useAuth()
+export const Permission = (props: IPermissionProps) => {
+  const {
+    RElement,
+    path,
+    checkFromParentPath,
+    checkPrivilege = [PRIVILEGEENUM.READ_LIST],
+  } = props
+  const { flatModulePropsFromURL, initDataFetching } = useAuth()
 
-//   console.log({ flatModulePropsFromURL })
+  const currentPathDetails = checkFromParentPath
+    ? flatModulePropsFromURL?.[checkFromParentPath]
+    : flatModulePropsFromURL?.[path]
 
-//   const currentPathDetails = checkFromParentPath
-//     ? flatModulePropsFromURL?.[checkFromParentPath]
-//     : flatModulePropsFromURL?.[path]
+  if (initDataFetching) {
+    return <FallbackLoader />
+  }
 
-//   if (initDataFetching) {
-//     return <>loading...</>
-//   }
+  if (currentPathDetails) {
+    const hasRouteAccess = !currentPathDetails?.isConfigurable
+      ? true
+      : checkPrivilege.some((checkPriv) =>
+          currentPathDetails?.resourceResponses
+            ?.map((priv) => priv.privilege)
+            .includes(checkPriv)
+        )
 
-//   if (currentPathDetails) {
-//     // const hasRouteAccess = currentPathDetails?.isConfigurable === false
-//     const hasRouteAccess = !currentPathDetails?.isConfigurable
-//       ? true
-//       : checkPrivilege.some((checkPriv) =>
-//           currentPathDetails?.privilege
-//             ?.map((priv) => priv.code)
-//             .includes(checkPriv)
-//         )
+    const routePrivilege = currentPathDetails?.resourceResponses?.reduce<
+      Partial<Record<PRIVILEGEENUM, boolean>>
+    >((currPrivilege, resource) => {
+      const mappedPrivilege = { ...currPrivilege }
+      mappedPrivilege[resource.privilege as PRIVILEGEENUM] = true
+      return mappedPrivilege
+    }, {})
 
-//     const routePrivilege = currentPathDetails?.privilege?.reduce<
-//       Partial<Record<PRIVILEGEENUM, boolean>>
-//     >((currPrivilege, privilege) => {
-//       const mappedPrivilege = { ...currPrivilege }
-//       mappedPrivilege[privilege.code] = true
-//       return mappedPrivilege
-//     }, {})
+    return hasRouteAccess ? (
+      <RouteWrapper>
+        <RElement routePrivilege={routePrivilege} />
+      </RouteWrapper>
+    ) : (
+      <NotFound>
+        <Text variant="small" color="text-cool-gray-600">
+          No permission
+        </Text>
+      </NotFound>
+    )
+  }
 
-//     return hasRouteAccess ? (
-//       <RouteWrapper>
-//         <RElement routePrivilege={routePrivilege} />
-//       </RouteWrapper>
-//     ) : (
-//       <NotFound>
-//         <Text variant="small" color="text-cool-gray-600">
-//           No permission
-//         </Text>
-//       </NotFound>
-//     )
-//   }
-
-//   return <NotFound />
-// }
+  return <NotFound />
+}
 
 export function createRoute<Type extends RouteType = 'private'>(
   args: CreateRoute<Type>
 ): _RouteObject<Type> {
+  const { element, path, checkFromParentPath, checkPrivilege } = args
   return {
     ...args,
-    element: (
-      <RouteWrapper>
-        <args.element />
-      </RouteWrapper>
-    ),
+    element:
+      args.type && ['public', 'bypass'].includes(args.type) ? (
+        <RouteWrapper>
+          <args.element />
+        </RouteWrapper>
+      ) : (
+        <Permission
+          RElement={element}
+          path={path as RoutePaths}
+          checkFromParentPath={checkFromParentPath}
+          checkPrivilege={checkPrivilege}
+        />
+      ),
     errorElement: <ErrorBoundary />,
   } as const
 }
