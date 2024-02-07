74408ed2111d2204a3fb6ef389d94a2d0833ac8c
Norbu Sherpa <nobusherpaa@gmail.com>
2024-02-07 08:51:47 +0545

    fip dynamic form

--- src/core/private/DynamicForm/DynamicFormModules/AddDynamicForm/AddDynamicForm.tsx
+++ src/core/private/DynamicForm/DynamicFormModules/AddDynamicForm/AddDynamicForm.tsx
@@ -17,120 +17,10 @@ import { useTranslation } from 'react-i18next'
 import { useLocation, useNavigate } from 'react-router-dom'
 import * as Yup from 'yup'
 
-// export const dynamicForm = [
-//   {
-//     id: 1,
-//     recommendationId: 11,
-//     recommendationName: 'फारम १',
-//     recommendationNameNp: 'From 1',
-//     fieldControlName: 'gender',
-//     fieldType: 'SELECT',
-//     orderNo: 1,
-//     isValidationRequired: true,
-//     labelNameEnglish: 'apple',
-//     labelNameNepali: 'applenp',
-//     className: 'ABC',
-//     fieldValidationList: [
-//       {
-//         id: 1,
-//         fieldId: 1,
-//         validationType: 'NOT_NULL',
-//         errorMessage: 'please enter value',
-//         regex: null,
-//       },
-//     ],
-//     dropDownResponse: {
-//       id: 3,
-//       dropDownCode: 'DROPDOWN2',
-//       dropDownDescriptionEn: 'bank setup',
-//       dropDownDescriptionNp: 'bank setup',
-//       isActive: true,
-//       dropDownDetailResponseDtoList: [
-//         {
-//           id: 26,
-//           dropDownId: 3,
-//           descriptionEn: 'Agriculture Dev Bank',
-//           descriptionNp: 'Agriculture Dev Bank',
-//           isActive: true,
-//           field1: 'bankType',
-//           value1: 'Development Bank',
-//         },
-//         {
-//           id: 25,
-//           dropDownId: 3,
-//           descriptionEn: 'Laxmi Bank',
-//           descriptionNp: 'Laxmi Bank Np',
-//           isActive: true,
-//           field1: 'bankType',
-//           value1: 'Commercial Bank',
-//         },
-//         {
-//           id: 27,
-//           dropDownId: 3,
-//           descriptionEn: 'Description Name',
-//           descriptionNp: 'Description Nepali',
-//           isActive: true,
-//           field1: 'fieldName1',
-//           value1: 'value1',
-//           field2: 'fieldName2',
-//           value2: 'value2',
-//           field3: 'fieldName3',
-//           value3: 'value3',
-//         },
-//       ],
-//     },
-//   },
-//   {
-//     id: 3,
-//     recommendationId: 11,
-//     recommendationName: 'फारम १',
-//     recommendationNameNp: 'From 1',
-//     fieldControlName: 'birthDate',
-//     fieldType: 'NEPALICALENDAR',
-//     orderNo: 1,
-//     isValidationRequired: true,
-//     labelNameEnglish: 'hello',
-//     labelNameNepali: 'hellonp',
-//     className: 'EFG',
-//     fieldValidationList: [],
-//     dropDownResponse: null,
-//   },
-//   {
-//     id: 2,
-//     recommendationId: 11,
-//     recommendationName: 'फारम १',
-//     recommendationNameNp: 'From 1',
-//     fieldControlName: 'firstName',
-//     fieldType: 'TEXT',
-//     orderNo: 3,
-//     isValidationRequired: true,
-//     labelNameEnglish: 'test',
-//     labelNameNepali: 'testnp',
-//     className: 'BCD',
-//     fieldValidationList: [
-//       {
-//         id: 1,
-//         fieldId: 2,
-//         validationType: 'NOT_NULL',
-//         errorMessage: 'please enter value',
-//         regex: null,
-//       },
-//       {
-//         id: 1,
-//         fieldId: 2,
-//         validationType: 'MAX_LENGTH',
-//         errorMessage: 'max length of 1 value',
-//         regex: null,
-//       },
-//     ],
-//     dropDownResponse: null,
-//   },
-// ]
-
 const AddDynamicForm = ({ currentModuleDetails }: Partial<IRoutePrivilege>) => {
   console.log({ currentModuleDetails })
   const location = useLocation()
-  
+
   const [isFormFieldReady, setIsFormFieldReady] = useState(false)
   const [validationSchema, setValidationSchema] = useState<Record<string, any>>(
     Yup.object({})
@@ -161,9 +51,10 @@ const AddDynamicForm = ({ currentModuleDetails }: Partial<IRoutePrivilege>) => {
     initialValues,
     validationSchema,
     onSubmit: (values) => {
-      console.log(values)
+      console.log(values, 'filter it')
     },
   })
+  console.log(formikConfig.values, 'filter here')
 
   return dynamicFormDataFetching ? (
     <FallbackLoader />
@@ -176,7 +67,9 @@ const AddDynamicForm = ({ currentModuleDetails }: Partial<IRoutePrivilege>) => {
         )}
         title={t('btns.add')}
         backAction={() => {
-          navigate(currentModuleDetails?.url || '-1', { state: { id: location?.state?.id!}})
+          navigate(currentModuleDetails?.url || '-1', {
+            state: { id: location?.state?.id! },
+          })
         }}
       />
       <ContainerLayout className="scrollbars grow ">
