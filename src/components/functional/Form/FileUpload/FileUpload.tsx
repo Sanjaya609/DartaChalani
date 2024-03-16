c1228f8c6436c8a12396698838892715642004c5
Norbu Sherpa <nobusherpaa@gmail.com>
2024-03-16 22:45:10 +0545

    fix: file upload disable in recommendation setup

--- src/components/functional/Form/FileUpload/FileUpload.tsx
+++ src/components/functional/Form/FileUpload/FileUpload.tsx
@@ -14,6 +14,7 @@ export interface IInputProps
   rightIcon?: React.ReactNode
   isNepali?: boolean
   canUploadMultipleFile?: boolean
+  disabled?: boolean
 }
 
 const FileUpload = forwardRef<HTMLInputElement, IInputProps>((props, ref) => {
@@ -36,6 +37,7 @@ const FileUpload = forwardRef<HTMLInputElement, IInputProps>((props, ref) => {
     isRequired,
     showError = true,
     canUploadMultipleFile = false,
+    disabled = false,
     ...rest
   } = props
   const hasErrorMessage = getErrorStatus({
@@ -157,6 +159,7 @@ const FileUpload = forwardRef<HTMLInputElement, IInputProps>((props, ref) => {
         </div>
 
         <input
+          disabled={disabled}
           type="file"
           accept={'jpg,png'}
           className="form-control sr-only"
